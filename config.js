4c23a27ec3c5e8f16dfe061c5da1dcd0126f12da
sarjounsd19 <sarjounsd2@gmail.com>
2024-04-16 21:05:50 +0300

    Initial Commit

---
+++ config.js
@@ -0,0 +1,11 @@
+url = "mongodb+srv://sarjounsd:1910Sarjoun@cluster0.2ttjfk4.mongodb.net/"
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose.connect(url)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error));
+app.listen(3000, () => console.log("Server is running"));
