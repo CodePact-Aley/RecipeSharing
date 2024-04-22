2ec2d0e7c3b2820a6cc37454d18c9cadbfb57f8e
fefe198 <abiormrahaf@gmail.com>
2024-04-22 20:47:13 +0200

    user controller

---
+++ Backend/controllers/user.js
@@ -0,0 +1,61 @@
+// const mongoose = require("mongoose");
+// const User = require('../models/User');
+
+// // Create a new user
+// exports.createUser = async (req, res) => {
+//     try {
+//         const newUser = await User.create(req.body);
+//         res.status(201).json(newUser);
+//     } catch (error) {
+//         res.status(400).json({ error: error.message });
+//     }
+// };
+
+// // Get all users
+// exports.getAllUsers = async (req, res) => {
+//     try {
+//         const users = await User.find();
+//         res.status(200).json(users);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// };
+
+// // Get user by ID
+// exports.getUserById = async (req, res) => {
+//     try {
+//         const user = await User.findById(req.params.id);
+//         if (!user) {
+//             return res.status(404).json({ message: 'User not found' });
+//         }
+//         res.status(200).json(user);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// };
+
+// // Update user by ID
+// exports.updateUserById = async (req, res) => {
+//     try {
+//         const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//         if (!updatedUser) {
+//             return res.status(404).json({ message: 'User not found' });
+//         }
+//         res.status(200).json(updatedUser);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// };
+
+// // Delete user by ID
+// exports.deleteUserById = async (req, res) => {
+//     try {
+//         const deletedUser = await User.findByIdAndDelete(req.params.id);
+//         if (!deletedUser) {
+//             return res.status(404).json({ message: 'User not found' });
+//         }
+//         res.status(204).send();
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// };
