f8461a7d4cfe9c4065c8e6d782821aa13e853a1a
fefe198 <abiormrahaf@gmail.com>
2024-04-22 20:06:54 +0200

    adding user,recipe,comment models

---
+++ Backend/model/comment.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  _id: {
+    type: Schema.Types.ObjectId // Corrected
+  },
+  recipe_id: {
+    type: Schema.Types.ObjectId // Corrected
+  },
+  commenter_id: {
+    type: Schema.Types.ObjectId // Corrected
+  },
+  commenter_photo: {
+    type: String // Assuming photo URL is stored as a string
+  },
+  comment_text: {
+    type: String // Corrected
+  },
+  comment_date: {
+    type: Date // Corrected
+  }
+});
+
+export default mongoose.model("Comment", commentSchema); // Changed model name to "Comment"
---
+++ Backend/model/user.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        _id: { 
+            type: Schema.Types.ObjectId // Corrected
+        },
+        username: { 
+            type: String, // Corrected
+            required: true 
+        },
+        email: { 
+            type: String, // Corrected
+            match: /^\\S+@\\S+\\.\\S+$/, // Changed pattern to match
+            required: true 
+        },
+        password_hash: { 
+            type: String, // Corrected
+            required: true 
+        },
+        join_date: { 
+            type: Date, // Corrected
+            required: true 
+        },
+        bio: { 
+            type: String // Corrected
+        },
+        profile_picture_url: { 
+            type: String // Corrected
+        },
+        favorites: {
+            type: [Schema.Types.ObjectId] // Corrected
+        }
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default mongoose.model("User", userSchema);
+
+
